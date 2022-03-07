7d542791c77ff6bd366ab0755ee38ae17fb1f332
Paul Lemus <paullemus@protonmail.com>
2022-03-06 18:54:01 -0800

    feat: User Form Submission with form_with
    View template has been upgraded to form_with tags.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def create
-    # @user = User.new(user_params)
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
@@ -12,7 +13,7 @@ class UsersController < ApplicationController
     end
   end
 
-  # def user_params
-  #   params.require(:user).permit(:username, :email, :password)
-  # end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
